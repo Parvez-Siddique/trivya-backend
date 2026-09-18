ccab92b515c34603fe7cb4121991415d61619df2
Parvez Siddique <parvezsiddique2369@gmail.com>
2026-09-18 22:50:21 +0530

    settings Change

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 ALLOWED_HOSTS = [
-    "trivya-backend-production.up.railway.app"
+    "trivya-backend-production.up.railway.app",
     "localhost",
     "127.0.0.1",
 ]
@@ -102,11 +102,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("PGDATABASE"),
-        "USER": os.getenv("PGUSER"),
-        "PASSWORD": os.getenv("PGPASSWORD"),
-        "HOST": os.getenv("PGHOST"),
-        "PORT": os.getenv("PGPORT", "5432"),
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT", "5432"),
     }
 }
 
