774940d08cfc7cca00feeb9f5b16317bb082b5f9
Parvez Siddique <parvezsiddique2369@gmail.com>
2026-09-19 16:23:49 +0530

    URL Change

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('trivyaserver.urls')),
+    path('', include('trivyaserver.urls')),
 ]
 
 if settings.DEBUG:
